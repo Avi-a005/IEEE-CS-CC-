03f1e0f88c39e10530a57846af70005601694129
Avishek Chowdhury <chowdhuryavishek9@gmail.com>
2025-03-07 22:17:05 +0530

    Add files via upload

---
+++ Level0.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* prev;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertHead(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head != NULL) {
+        (*head)->prev = newNode;
+        newNode->next = *head;
+    }
+    *head = newNode;
+}
+
+void insertTail(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+void traverseForward(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+void traverseBackward(struct Node* head) {
+    if (head == NULL) return;
+    struct Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->prev;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+    int choice, data;
+
+    while (1) {
+        printf("\n1. Insert at head\n");
+        printf("2. Insert at tail\n");
+        printf("3. Traverse forward\n");
+        printf("4. Traverse backward\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter data to insert at head: ");
+                scanf("%d", &data);
+                insertHead(&head, data);
+                break;
+            case 2:
+                printf("Enter data to insert at tail: ");
+                scanf("%d", &data);
+                insertTail(&head, data);
+                break;
+            case 3:
+                printf("Forward Traversal:\n");
+                traverseForward(head);
+                break;
+            case 4:
+                printf("Backward Traversal:\n");
+                traverseBackward(head);
+                break;
+            case 5:
+                exit(0);
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    }
+
+    return 0;
+}
